a1c45fdadd640339b6cba5a0cffbc51a53927211
ptejalaxioned <tejalp@axioned.com>
2024-05-27 16:01:01 +0530

    html correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-/* Author: 
-
-*/
+let li =document.querySelectorAll("li")
 
 
+li.forEach((val,index)=>{
+   li[index].addEventListener("click",(e)=>{
+console.log(e.target.className)
+if(e.target.className==="london"){
+    console.log("London")
+}
 
+})
 
-
-
-
-
+})
 
 
 
